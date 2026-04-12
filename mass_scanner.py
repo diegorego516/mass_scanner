1667e1f79bc7494fb972e0c0eb76921e76f8b108
DiegoDev <diegorego516@gmail.com>
2026-04-12 16:37:18 -0700

    Atualizar o mass_scanner.py

--- mass_scanner.py
+++ mass_scanner.py
@@ -1,4 +1,5 @@
-import os                                                   import sys
+import os                       
+import sys
 import nmap                                                 
 def nmap(os,sys):
 
