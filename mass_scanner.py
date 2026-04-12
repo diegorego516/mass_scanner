872f2af33b2c0301c0c50fc76cd113360ac7770b
* <diegorego516@gmail.com>
2026-04-11 23:23:07 -0300

    minha atuali,ação

---
+++ mass_scanner.py
@@ -0,0 +1,24 @@
+import nmap
+import os
+import sys
+
+def nmap3(os, sys):
+
+		if port in ports: 
+
+			criar = mass_scanner()
+def init_mass_scanner(os,sys):
+	self = init_mass_scanner(os,sys)
+print("\nInicialisando o mass_scanner...")
+escolha = input ("Escolha as opções de ('1-3'): ")
+print("1. Escanear Portas: ('1-65535'): ")
+print("2. Verificar se há portas abertas: ")
+print("3. Sair: ")
+sys.exit(0)
+print("Saindo...")
+if escolha_input("int(input == ('1-3'): "):
+	input("1. Escanear Portas: ('1-65535'): ")
+	input("2. Verificar se há portas abertas: ")
+	input("3. Sair: ")
+	sys.exit(1)
+	print("Saindo do sistema...")
