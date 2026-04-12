e30645f6e1437a0bdc3340f8b50db407ea4050ee
DiegoDev <diegorego516@gmail.com>
2026-04-12 16:36:19 -0700

    Atualizar o mass_scanner.py

--- mass_scanner.py
+++ mass_scanner.py
@@ -1,33 +1,20 @@
-import os
-import sys
-import nmap
+import os                                                   import sys
+import nmap                                                 
+def nmap(os,sys):
 
-def port():
+        if nmap in ports:
 
-        if port in nmap:
-
-                def nmap(os, sys):
+                def nmap(os,sys):
 # Aqui é onde nós criamos a função do mass_scanner
 
                         criar = mass_scanner()
 def mass_scanner():
-
-        if mass_scanner in ports(nmap):
-
-                mass_scan(in_ports);
-
-def scan():
-                nmap = scan in open_ports("1-65535");
-
-def opção():
-
-                input = opção ("Escolha a opção de ('1-5'): ")
-                print ("inicializando o mass scanner...")
-                input = opção("1-5")
-                input = opção("1. Escanear Por Portas Abertas (1-65535): ")
-                input = opção("2. Verificar se há vulnerabilididades em sistemas operacionais: ")
-                input = opção("3. Gerar um relatório de log ('Y/N'): ")
-                input = opção("4. Enviar o relatório detalhado de vulnerabilidades para especialistas de cibersegurança: ")
+                                                                    if mass_scanner in ports(nmap):
+                                                                            mass_scan(in_ports);
+                                                            def scan():                                                                 nmap = scan in open_ports("1-65535")                        print("Escolha uma opção de 1-5")                           input = opção("1. Escanear Por Portas Abertas ('1-65535'): ")
+                input = opção("2. Verificar se há vulnerabilidades nas portas abertas? ('S/N'): ")
+                input = opção("3. Gerar um relatório de log? ('S/N'): ")
+                input = opção("4. Enviar o relatório detalhado para especialistas de cíbersegurança? ('S/N'): ")
                 input = opção("5. Sair: ")
                 sys.exit(0)
                 print("Saindo...")
