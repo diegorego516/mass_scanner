4d53e3cec054cb7562883c24b12a6b895c6546bb
DiegoDev <diegorego516@gmail.com>
2026-04-12 15:47:59 -0700

    Update mass_scanner.py

--- mass_scanner.py
+++ mass_scanner.py
@@ -9,24 +9,25 @@ def port():
                 def nmap(os, sys):
 # Aqui é onde nós criamos a função do mass_scanner
 
-                        scriar_função, criar = mass_scanner()
+                        criar = mass_scanner()
 def mass_scanner():
 
-        if mass_scanner in ports (nmap):
+        if mass_scanner in ports(nmap):
 
-         mass_scan(in_ports);
+                mass_scan(in_ports);
 
 def scan():
+                nmap = scan in open_ports("1-65535");
 
-        nmap = scan in open_ports(1-65535);
+def opção():
 
-opções = input ("Escolha a opção de ('1-5'): ")
-print("\nInicialisando o mass scanner...")
-if input == opções("1-5"):
-        input = opção("1. Escanear Por Portas Abertas (1-65535): ")
-        input = opção("2. Verificar se há vulnerabilidades em sistemas operacionais: ")
-        input = opção("3. gerar um relatório de log ('Y/N'): ")
-        input = opção("4. Enviar o relatório detalhado de vulnerabilidades para especialistas em cibersegurança: ")
-        input = opção("5. Sair: ")
-        sys.exit(0)
-        print("Saindo...")
+                input = opção ("Escolha a opção de ('1-5'): ")
+                print ("inicializando o mass scanner...")
+                input = opção("1-5")
+                input = opção("1. Escanear Por Portas Abertas (1-65535): ")
+                input = opção("2. Verificar se há vulnerabilididades em sistemas operacionais: ")
+                input = opção("3. Gerar um relatório de log ('Y/N'): ")
+                input = opção("4. Enviar o relatório detalhado de vulnerabilidades para especialistas de cibersegurança: ")
+                input = opção("5. Sair: ")
+                sys.exit(0)
+                print("Saindo...")
