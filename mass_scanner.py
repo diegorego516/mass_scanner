bc7076f48e020f8e6322202fe03ca362a3a6e6e4
DiegoDev <diegorego516@gmail.com>
2026-04-12 17:50:33 -0700

    Update mass_scanner.py

--- mass_scanner.py
+++ mass_scanner.py
@@ -2,24 +2,13 @@ import os
 import sys
 import nmap
 
-nmap = mass_scanner(os,sys)
-
-if nmap in ports:
-
-nmap = mass_scanner.scan(open_ports)
-nmap = mass_scanner.recon(network_ipv4)
-nmap = mass_scanner.audit(search_vulnerability_databases)
-
-if mass_scanner in ports:
-create = mass_scanner()
-
-if mass_scanner in vulnerabilities:
-start_server = ("127.0.0.1")
-start_client = ("5000")
-
-if __name__ == "__main__":
-   start_server():
-
-commands = mass_scanner.py(ip_scanner):
-commands = mass_scanner.py(recon_audit):
-commands = mass_scanner.py(network_analyzer):
+print("\nInitializing Mass Scanner...")
+input("Choose From options ('1-5'): ")
+input("1. Scan for Open Ports ('1-65535'): ")
+input("2. Generate a detailed vulnerability report log? ('Y/N'): ")
+input("3. Send in the detailed vulnerability report log? ('Y/N'): ")
+input("4. Ensure that the Mass Scanner scans for Open_Ports properly and regularly from time to time in order to avoid potential leaks on your system? (Y/N):  ")
+input("5. Exit: ")
+sys.exit(0)
+print("exiting...")
+System_On_Exit("Clear")
